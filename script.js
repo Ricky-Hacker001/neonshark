e510ee16516480616b8b2a6b7d4bfb4f0b0022fa
ricky <ricky@Ricky-kali.com>
2024-12-16 12:26:44 +0530

    Initial commit of neonshark folder

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Frame 755 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 54px;
+
+position: absolute;
+width: 628px;
+height: 451px;
+left: 78px;
+top: 437px;
+
